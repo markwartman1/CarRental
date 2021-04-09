283435bc9d4d774f45f4471d6d0d7206834816a9
markwartman1 <markwartman1@gmail.com>
2021-04-08 23:07:19 -0500

    added onDestroy to garage/HomeComponent

--- src/app/garage/home/home.component.ts
+++ src/app/garage/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MessageService } from '../service/message.service';
 
@@ -7,16 +7,19 @@ import { MessageService } from '../service/message.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   message: string = "default";
   subscription: Subscription;
 
   constructor(public messageService: MessageService) { }
-
+  
   ngOnInit(): void {
-
+    
     this.subscription = this.messageService.message.subscribe(mess => this.message = mess);
   }
-
+  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
