94b3f6faa5743e1a9db266e14c6f1cfd92eee9c6
markwartman1 <markwartman1@gmail.com>
2021-05-25 14:26:30 -0500

    card update

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 const Material = [
-  MatCardModule
+  MatCardModule,
+  MatGridListModule
 ];
 
 @NgModule({
